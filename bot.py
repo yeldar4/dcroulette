6b880aaf9c5e209599f44bd7782a97f34f6089cf
yeldar4 <benradley4@gmail.com>
2023-06-29 10:33:45 -0400

    Added some comments

--- bot.py
+++ bot.py
@@ -6,19 +6,30 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
+# Retrieves the .env values for the bot's token, server, and server ID 
+# respectively
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 GUILD_ID = os.getenv('GUILD_ID')
+# Denotes the prefix to use the bot in a text channel, in this case '+'
 bot = commands.Bot(command_prefix='+',intents=discord.Intents.all())
 
+# Bot command that randomly selects a user in the voice channel of 
+# whoever initiated the command, and disconnects them
 @bot.command(name='spin')
 async def spin(ctx):
     guild = bot.get_guild(int(GUILD_ID))
     vc = ctx.author.voice.channel
+    # Checks that the text author is in a valid VC
     if vc:
+        # Gets list of voice states in selected voice channel. Voice 
+        # states represent the active connection of a user to the
+        # voice channel
         voice_states = vc.voice_states
         if len(voice_states) > 0:
             unlucky = random.choice(list(voice_states))
+            # gets voice_client of selected user by id (unlucky)
             victim = guild.get_member(unlucky)
+            # Disconnects user
             await victim.move_to(None)
 bot.run(TOKEN)
